d39a2f6bb68a8b8fff05025624afa6a15a7095a4
jarrodskahill <jarrodskahill@gmail.com>
2021-02-23 11:33:58 -0600

    added projects to landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../scss/landing.scss";
 import codingImg from "../imgs/coding_pic.jpg";
+import ProjectDisplay from "./projects/projectDisplay";
 const LandingPage = () => {
   return (
     <div className="landing">
@@ -8,6 +9,7 @@ const LandingPage = () => {
         <img src={codingImg} />
         <h2>Hello my name is Jarrod Skahill, and welcome to my Portfolio!</h2>
       </div>
+      <ProjectDisplay />
     </div>
   );
 };

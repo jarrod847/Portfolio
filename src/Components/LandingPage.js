ce4687ed2c43d774336a396b6d1bd985e2369bf8
jarrodskahill <jarrodskahill@gmail.com>
2021-02-23 10:40:04 -0600

    added a picture to header of landing page and changed some styles

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,20 +1,12 @@
 import React from "react";
 import "../scss/landing.scss";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import codingImg from "../imgs/coding_pic.jpg";
 const LandingPage = () => {
   return (
     <div className="landing">
-      <div className="landing-left">
+      <div className="landingHeaderContainer">
+        <img src={codingImg} />
         <h2>Hello my name is Jarrod Skahill, and welcome to my Portfolio!</h2>
-        {/* <p>
-          I have three pages: <Link className="land-links">About</Link> for a
-          short summary about myself,{" "}
-          <Link className="land-links">Projects</Link> is where you can find
-          current and past work, and <Link className="land-links">Social</Link>{" "}
-          is where I provide links to my social platforms and where you can send
-          me your email with inquires. Thanks for stopping by, feel free to
-          reach out and get in touch through my social page.
-        </p> */}
       </div>
     </div>
   );

e10e8762c55ce25be32b333ba63e8d18af931f21
jarrodskahill <jarrodskahill@gmail.com>
2021-02-23 13:34:38 -0600

    created a component for the front of the card

---
+++ src/Components/projects/projectFront.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProjectFront = () => {
+  return (
+    <div className="project">
+      <h1>{data.project}</h1>
+      <p>Stack: {data.stack}</p>
+      <p>Start Date: {data.start}</p>
+      <p>End Date: {data.end}</p>
+      <div className="projectLinks">
+        <a href={data.github}>Github Link</a>
+        <a href={data.deployed}>Deployed Link</a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectFront;

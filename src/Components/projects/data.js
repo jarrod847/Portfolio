6d029ada1f40de749111a67203d8cc8a3577ffa7
jarrodskahill <jarrodskahill@gmail.com>
2021-02-24 18:58:11 -0600

    added descriptions to projects so its shown on flip

--- src/Components/projects/data.js
+++ src/Components/projects/data.js
@@ -6,6 +6,8 @@ export const data = [
     end: "03-2020",
     deployed: "",
     github: "",
+    description:
+      "An application to organize data for a non-profit organization that helps foster parent's recieve help from others outside of the home",
   },
   {
     project: "Fanatics Gym",
@@ -14,6 +16,8 @@ export const data = [
     end: "Present",
     deployed: "",
     github: "https://github.com/Fanatics-Gym/Front-End",
+    description:
+      "An E-commerce site, that promotes fitness and personal well-being. The sight also handles the application process for a football league where users can apply to play in the league",
   },
   {
     project: "Tracker",
@@ -22,6 +26,7 @@ export const data = [
     end: "Present",
     deployed: "",
     github: "",
+    description: "Multi purpose tracker, that has two lists",
   },
   {
     project: "Timer",
@@ -30,6 +35,8 @@ export const data = [
     end: "06-2020",
     deployed: "https://timer-two.vercel.app/",
     github: "https://github.com/jarrod847/timer",
+    description:
+      "A timer were you can set the amount of mins or seconds it will count down from. The sight also offers a stop-watch for users to time themselves, being able to stop and start at anytime",
   },
   {
     project: "Conways Game of Life",
@@ -38,5 +45,7 @@ export const data = [
     end: "05-2020",
     deployed: "https://gameoflife-jarrodskahill.now.sh/",
     github: "https://github.com/jarrod847/GameOfLife",
+    description:
+      "A game that runs by itself, click a few squares and click start and watch it follow a set of rules",
   },
 ];
